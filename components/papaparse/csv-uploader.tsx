6b9d4f4b23cb73c12e65c81134a684f71ead5562
ShalahuddinAbdul <89372431+1CL0UD@users.noreply.github.com>
2024-08-06 09:09:33 +0700

    styling csv uploader

--- components/papaparse/csv-uploader.tsx
+++ components/papaparse/csv-uploader.tsx
@@ -32,10 +32,13 @@ const CSVUploader = ({ onDataUploaded }: CSVUploaderProps) => {
         getRemoveFileProps,
       }: any) => (
         <>
-          <div className="flex flex-row">
+          <div className="flex flex-row space-x-4">
             <Button {...getRootProps()}>Browse file</Button>
-            <div className="flex items-center justify-start border w-full h-10 px-2">
-              {acceptedFile && acceptedFile.name}
+            <div
+              {...getRootProps()}
+              className="flex items-center justify-start h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors cursor-pointer file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 hover:text-slate-500 hover:scale-[1.001]"
+            >
+              {acceptedFile ? acceptedFile.name : 'Click Here to Upload CSV'}
             </div>
             <Button variant="destructive" {...getRemoveFileProps()}>
               Remove
