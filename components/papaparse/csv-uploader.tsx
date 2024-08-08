064db08d4174fce9ef59f7b6a6c201051809c512
ShalahuddinAbdul <89372431+1CL0UD@users.noreply.github.com>
2024-08-08 17:33:28 +0700

    made hero for home page

--- components/papaparse/csv-uploader.tsx
+++ components/papaparse/csv-uploader.tsx
@@ -17,37 +17,42 @@ const CSVUploader = ({ onDataUploaded }: CSVUploaderProps) => {
   const { CSVReader } = useCSVReader();
 
   return (
-    <CSVReader
-      onUploadAccepted={(results: any) => {
-        console.log('---------------------------');
-        console.log(results);
-        console.log('---------------------------');
-        onDataUploaded(results);
-      }}
-    >
-      {({
-        getRootProps,
-        acceptedFile,
-        ProgressBar,
-        getRemoveFileProps,
-      }: any) => (
-        <>
-          <div className="flex flex-row space-x-4">
-            <Button {...getRootProps()}>Browse file</Button>
-            <div
-              {...getRootProps()}
-              className="flex items-center justify-start h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors cursor-pointer file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 hover:text-slate-500 hover:scale-[1.001]"
-            >
-              {acceptedFile ? acceptedFile.name : 'Click Here to Upload CSV'}
+    <div className="flex flex-col space-y-12">
+      <div className="container flex justify-center bg-blue-400 text-white rounded-lg p-12">
+        <h1 className="text-7xl">Upload your CSV file here.</h1>
+      </div>
+      <CSVReader
+        onUploadAccepted={(results: any) => {
+          console.log('---------------------------');
+          console.log(results);
+          console.log('---------------------------');
+          onDataUploaded(results);
+        }}
+      >
+        {({
+          getRootProps,
+          acceptedFile,
+          ProgressBar,
+          getRemoveFileProps,
+        }: any) => (
+          <>
+            <div className="flex flex-row space-x-4">
+              <Button {...getRootProps()}>Browse file</Button>
+              <div
+                {...getRootProps()}
+                className="flex items-center justify-start h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors cursor-pointer file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 hover:text-slate-500 hover:scale-[1.001]"
+              >
+                {acceptedFile ? acceptedFile.name : 'Click Here to Upload CSV'}
+              </div>
+              <Button variant="destructive" {...getRemoveFileProps()}>
+                Remove
+              </Button>
             </div>
-            <Button variant="destructive" {...getRemoveFileProps()}>
-              Remove
-            </Button>
-          </div>
-          <ProgressBar style={styles.progressBarBackgroundColor} />
-        </>
-      )}
-    </CSVReader>
+            <ProgressBar style={styles.progressBarBackgroundColor} />
+          </>
+        )}
+      </CSVReader>
+    </div>
   );
 };
 
--- constants/constants.ts
+++ constants/constants.ts
@@ -1,5 +1,4 @@
 export const navbarMenu = [
-  { label: 'Official Website', value: 'official-website' },
   { label: 'Linkedin Profile', value: 'linkedin-profile' },
   { label: 'Employee Linkedins', value: 'employee-linkedins' },
 ];
